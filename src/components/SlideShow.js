90b74d06843ab4892c1d9960aef2f00eb6be3b04
Coyeah <coyeah_chen@outlook.com>
2018-06-04 23:54:20 +0800

    update: v0.2.2

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -15,8 +15,15 @@ class SlideShow extends Component {
   constructor(props) {
     super(props);
 
+    this.slideTouchStart = this.slideTouchStart.bind(this);
+    this.slideTouchMove = this.slideTouchMove.bind(this);
+    this.slideTouchEnd = this.slideTouchEnd.bind(this);
+
     this.state = {
-      
+      imageIndex: 0,
+      mouseX: 0,
+      mouseLeft: 0,
+      isTouch: false,
     };
   }
 
@@ -30,13 +37,82 @@ class SlideShow extends Component {
     );
   }
 
+  getPointList() {
+    return (
+      this.props.children.map((value, index) => {
+        if (index == this.state.imageIndex) {
+          let style = {
+            "background-color": "#c20c0c",
+          }
+
+          return (
+            <li style={style}></li>
+          );
+        }
+
+        return (
+          <li></li>
+        );
+      })
+    );
+  }
+
+  slideTouchStart(e) {
+    this.setState({
+      mouseX: e.touches[0].clientX,
+      isTouch: true,
+    });
+  }
+
+  slideTouchMove(e) {
+    this.setState({
+      mouseLeft: e.touches[0].clientX - this.state.mouseX,
+    });
+  }
+
+  slideTouchEnd() {
+    console.log(this.state.mouseLeft);
+    let imageIndex = this.state.imageIndex;
+    if (this.state.mouseLeft < (-1 * window.screen.width * 0.5)) {
+      imageIndex++
+    } else if (this.state.mouseLeft > (window.screen.width * 0.5)) {
+      imageIndex--;
+    }
+    this.setState({
+      imageIndex,
+      mouseX: 0,
+      mouseLeft: 0,
+      isTouch: false,
+    });
+  }
+
   render() {
     const { classPrefix } = this.props;
+    const cxImage = `${classPrefix}-image`;
+    const cxPoint = `${classPrefix}-point`;
+
+    const styleDiv = {
+      height: window.screen.width * 336 / 730,  
+    };
+
+    let styleUl = null;
+    if (this.state.isTouch) {
+      styleUl = {
+        left: this.state.mouseLeft,
+      };
+    } else {
+      styleUl = {
+        left: this.state.imageIndex * window.screen.width * -1,
+      };
+    }
 
     return (
-      <div className={classPrefix}>
-        <ul>
+      <div className={classPrefix} style={styleDiv}>
+        <ul className={cxImage} style={styleUl} onTouchStart={this.slideTouchStart} onTouchMove={this.slideTouchMove} onTouchEnd={this.slideTouchEnd}>
           {this.getImageList()}
+        </ul>
+        <ul className={cxPoint}>
+          {this.getPointList()}
         </ul>      
       </div>
     );
